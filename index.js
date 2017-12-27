6f7f04cc9ef6c033965c698989624815f8fce6cc
Oleg Lazarenko <metamorph_online@yahoo.com>
2017-12-27 15:56:00 +0200

    adding google oauth

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
 const app = express();
 
-
-app.get('/', (req, res) => {
-
-	res.send({ hi: 'there'});
-});
+//clientid = 431497791297-8gp911q3160qod51fhih90n7flvkimth.apps.googleusercontent.com
+//client secret = MSk3AYC19M_j2dHo6UbffneP
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
