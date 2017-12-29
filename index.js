80cbe49b17151e768b6ba4327f596ef1d236391b
Oleg Lazarenko <metamorph_online@yahoo.com>
2017-12-29 18:09:57 +0200

    google auth

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const keys = require('./config/keys');
 
 const app = express();
 
-//clientid = 431497791297-8gp911q3160qod51fhih90n7flvkimth.apps.googleusercontent.com
-//client secret = MSk3AYC19M_j2dHo6UbffneP
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'},
+	(accessToken) => {
+		console.log(accessToken);
+	}
+));
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}));
+
 
 const PORT = process.env.PORT || 5000;
 
