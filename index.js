ef394d9d12e6c97e198f10ed0b1056b268366133
metamorph-online <metamorph_online@yahoo.com>
2018-01-02 01:39:43 +0200

    code review

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+const authRouts = require('./routes/authRoutes');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret,
-	callbackURL: '/auth/google/callback'},
-	(accessToken) => {
-		console.log(accessToken);
-	}
-));
 
-app.get('/auth/google', passport.authenticate('google', {
-	scope: ['profile', 'email']
-}));
-
+authRouts(app);
 
 const PORT = process.env.PORT || 5000;
 
